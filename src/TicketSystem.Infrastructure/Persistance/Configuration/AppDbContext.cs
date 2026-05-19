fdd6a0de5ec8d92ebe17bf6b8cf49d965c1c34ff
emenesism <mhdnris@gmail.com>
2026-05-19 17:14:59 +0330

    feat(infrastructure): add notifications db set

--- src/TicketSystem.Infrastructure/Persistance/Configuration/AppDbContext.cs
+++ src/TicketSystem.Infrastructure/Persistance/Configuration/AppDbContext.cs
@@ -12,6 +12,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options)
     public DbSet<TicketMessage> TicketMessages { get; set; }
     public DbSet<Session> Sessions { get; set; }
     public DbSet<Attachment> Attachments { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)

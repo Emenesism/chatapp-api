bcdda1c5baa7c78750c5a46b9f2a9d694a0ad166
emenesism <mhdnris@gmail.com>
2026-05-19 17:14:59 +0330

    feat(infrastructure): add notification repository

---
+++ src/TicketSystem.Infrastructure/Persistance/Repositories/NotificationRepository.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using TicketSystem.Application.Abstractions.Repositories;
+using TicketSystem.Domain.Entities;
+using TicketSystem.Infrastructure.Persistance.Configuration;
+
+namespace TicketSystem.Infrastructure.Persistance.Repositories;
+
+
+
+public class NotificationRepository(AppDbContext db) : INotificationRepository
+{
+    private readonly AppDbContext _db = db;
+
+    public async Task AddAsync(Notification notification)
+    {
+        await _db.Notifications.AddAsync(notification);
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task<List<Notification>> GetUserNotification(string userId)
+    {
+        return await _db.Notifications
+        .AsNoTracking()
+        .Where(s =>
+            s.UserId == userId &&
+            s.IsRead == false)
+        .OrderByDescending(s => s.CreatedAt)
+        .ToListAsync();
+    }
+}

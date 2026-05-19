015c40bb8e4c730d4f78762326107d6556c66567
emenesism <mhdnris@gmail.com>
2026-05-19 17:14:59 +0330

    feat(application): add notification repository abstraction

---
+++ src/TicketSystem.Application/Abstractions/Repositories/INotificationRepository.cs
@@ -0,0 +1,13 @@
+
+
+using TicketSystem.Domain.Entities;
+
+namespace TicketSystem.Application.Abstractions.Repositories;
+
+
+
+public interface INotificationRepository
+{
+    Task AddAsync(Notification notification);
+    Task<List<Notification>> GetUserNotification(string userId);
+}

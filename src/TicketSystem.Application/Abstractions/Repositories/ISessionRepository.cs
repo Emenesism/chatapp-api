4297bbeb40e31eb5a418c859668344f23661f1ce
emenesism <mhdnris@gmail.com>
2026-05-20 13:31:43 +0330

    refactor(application): return sessions from dashboard repo

--- src/TicketSystem.Application/Abstractions/Repositories/ISessionRepository.cs
+++ src/TicketSystem.Application/Abstractions/Repositories/ISessionRepository.cs
@@ -1,4 +1,3 @@
-using TicketSystem.Application.Dtos.Dashboard;
 using TicketSystem.Domain.Entities;
 
 namespace TicketSystem.Application.Abstractions.Repositories;
@@ -19,5 +18,5 @@ public interface ISessionRepo
     Task RevokeAllUserSessions(Guid userId);
     Task RevokeAllAdminSessions(Guid adminId);
     Task<Session?> ReuseDetector(string tokenHash);
-    Task<List<AllSessionResponse>> GetActiveSessionForDashboard();
+    Task<List<Session>> GetActiveSessionForDashboard();
 }

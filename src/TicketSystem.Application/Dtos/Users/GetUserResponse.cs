dcd277c3dbad4056d10d2583a5aa46c65eda283d
emenesism <mhdnris@gmail.com>
2026-05-20 13:32:21 +0330

    refactor(application): remove user response dto

--- src/TicketSystem.Application/Dtos/Users/GetUserResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace TicketSystem.Application.Dtos.Users;
-
-public sealed class GetUserResponse
-{
-    public string Name { get; set; } = string.Empty;
-    public string Username { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
-}

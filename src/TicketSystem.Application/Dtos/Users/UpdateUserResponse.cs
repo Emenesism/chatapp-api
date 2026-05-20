e3740f968f432db90a5d7921d4c74ae72edf2878
emenesism <mhdnris@gmail.com>
2026-05-20 13:32:22 +0330

    refactor(application): remove user update response dto

--- src/TicketSystem.Application/Dtos/Users/UpdateUserResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace TicketSystem.Application.Dtos.Users;
-
-public class UpdateUserResponse
-{
-    public string Name { get; set; } = string.Empty;
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-}

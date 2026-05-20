758744ff8586e9fb03457b826632f2522675e5bd
emenesism <mhdnris@gmail.com>
2026-05-20 13:32:14 +0330

    refactor(application): remove ticket response dto

--- src/TicketSystem.Application/Dtos/Tickets/TicketResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace TicketSystem.Application.Dtos.Tickets;
-
-public class TicketResponse
-{
-    public Guid Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; }
-    public bool IsSolved { get; set; }
-}

f3aaf5331dcc77bba2696aa44dc877a7156865b4
emenesism <mhdnris@gmail.com>
2026-05-20 13:31:51 +0330

    refactor(application): remove admin update response dto

--- src/TicketSystem.Application/Dtos/Admins/UpdateAdminResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace TicketSystem.Application.Dtos.Admins;
-
-public class UpdateAdminResponse
-{
-    public string Name { get; set; } = string.Empty;
-    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-}
